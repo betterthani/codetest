bca0af8e2525bd618800919ae43301b4983a747b
betterthani <117792875+betterthani@users.noreply.github.com>
2024-02-22 12:53:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,18 +1,10 @@
-import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int[] answer = new int[10];
-        int sum = 0;
-        
-        for(int i = 0; i<numbers.length; i++){
-            answer[numbers[i]] = 1;
+        int sum = 45; // 0~9까지의 합
+        for(int i = 0; i < numbers.length; i++){
+            sum -= numbers[i];
         }
         
-        for(int i = 0; i < answer.length; i++){
-            if(answer[i] == 0){
-                sum += i;
-            }
-        }
         return sum;
     }
 }
