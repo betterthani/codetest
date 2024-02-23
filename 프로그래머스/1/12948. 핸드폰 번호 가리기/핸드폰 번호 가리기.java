6dc00de8c62eca7d4c31e25b9f8c04142b97821a
betterthani <117792875+betterthani@users.noreply.github.com>
2024-02-23 13:38:14 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.66 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String phone_number) {
+        
+        // 뒷 4자리
+        String backNumber = phone_number.substring(phone_number.length()-4, phone_number.length());
+        
+        // 뒷 4자리 제외 앞자리
+        phone_number = phone_number.substring(0, phone_number.length()-4);
+        
+        for(int i = 0; i < phone_number.length(); i++){
+            phone_number = phone_number.replace(phone_number.substring(i,i+1), "*");
+        }
+        
+        String answer = phone_number + backNumber;
+        
+        return answer;
+    }
+}
