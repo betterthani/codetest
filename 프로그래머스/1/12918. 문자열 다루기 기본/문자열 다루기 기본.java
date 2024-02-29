29a58342bb791ac5ce4b39b384497b47906002de
betterthani <117792875+betterthani@users.noreply.github.com>
2024-02-29 13:37:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        
+        if(s.length() == 4 || s.length() == 6){
+            try{
+                int x = Integer.parseInt(s);
+                return answer;
+            }catch(NumberFormatException e){
+                return !answer;
+            }
+        } else 
+        
+        return !answer;
+    }
+}
