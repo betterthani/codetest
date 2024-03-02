207ddc2fc60060dcd64b5233281b55451d336aa8
betterthani <117792875+betterthani@users.noreply.github.com>
2024-03-02 12:44:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.82 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        int value = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != value){
+                answer.add(arr[i]);
+            }    
+            value = arr[i];
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+        
+
+    }
+}
