753e446b2b4b4effeebfedf80efce9ab0b327589
betterthani <117792875+betterthani@users.noreply.github.com>
2024-08-08 12:15:23 +0900

    [level 2] Title: 올바른 괄호, Time: 8.79 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,22 +1,18 @@
+from collections import deque
 def solution(s):
-    answer = True
-    
-    stack = []
+    d = deque()
     
     for i in s :
         if i == '(' :
-            stack.append('(')
+            d.append(i)
         else :
-            if stack == [] :
-                # stack 이 비어있을 경우
+            if len(d) == 0 :
                 return False
             else :
-                # ')'로 들어갈 경우
-                stack.pop() # '(' 빼내기
-    if stack != [] :
-        # stack이 비어있지 않을 때 -> 짝이 맞지 않을 때
-        return False
-    else :
-        return True
+                d.pop()
         
+    if len(d) == 0 :
+        return True
+    else :
+        return False
         
